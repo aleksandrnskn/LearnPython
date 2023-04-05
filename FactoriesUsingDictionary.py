468b81887a800dcc7f9215aafbfa5a4fa39f30c7
Aleksandr NSKN <71671711+aleksandrnskn@users.noreply.github.com>
2023-04-06 00:40:34 +0300

    Create FactoriesUsingDictionary.py

---
+++ FactoriesUsingDictionary.py
@@ -0,0 +1,35 @@
+from typing import Hashable, Callable
+
+
+class ClassNotFoundError(ValueError):
+    ...
+
+
+class SubjectOne(object):
+    ...
+
+
+class SubjectTwo(object):
+    ...
+
+
+class Factory(object):
+    @staticmethod
+    def get(class_name: Hashable) -> object:
+        if not isinstance(class_name, Hashable):
+            raise ValueError("class_name must be a Hashable type!")
+        
+        classes: dict[Hashable, Callable[..., object]] = {
+            "SubjectOne": SubjectOne,
+            "SubjectTwo": SubjectTwo
+        }
+
+        class_ = classes.get(class_name, None)
+        if class_ is not None:
+            return class_
+
+        raise ClassNotFoundError
+
+
+# Usage
+class_ = Factory.get("SubjectOne"
